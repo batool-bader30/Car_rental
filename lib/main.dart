d67db509585d1c83c10af90fafba426768d04f94
ayamaroof10420 <talataya035@gmail.com>
2025-11-10 18:57:20 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:carrental/controllers/car_provider.dart';
 
 void main() async {
   // ضمان تهيئة Flutter قبل أي استدعاء Firebase أو async
